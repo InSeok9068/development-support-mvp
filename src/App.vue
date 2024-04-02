a179d32f3ccb56a1308c9c88ef1a68ee1bed363a
Lee In Seok <dlstjr9068@gmail.com>
2024-04-03 00:30:56 +0900

    로그인 유도 작업

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,13 @@ import AppModal from '@/components/app/AppModal.vue';
 import { useSign } from '@/composables/sign';
 import TheLayoutNavi from '@/layouts/TheLayoutNavi.vue';
 import TheLaytout from '@/layouts/TheLaytout.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const { isAuth } = useSign();
 
 isAuth.value = !!localStorage.getItem('pocketbase_auth');
+
+!isAuth.value && router.push('/sign');
 </script>
