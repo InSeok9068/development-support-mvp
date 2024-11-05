7a48f2bfe278599c20fbea44e8a37819030349ed
Lee In Seok <dlstjr9068@gmail.com>
2024-11-05 19:33:17 +0900

    빌드 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import Vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import Components from 'unplugin-vue-components/vite';
+import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 import VueDevTools from 'vite-plugin-vue-devtools';
-import VueRouter from 'unplugin-vue-router/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,9 +16,6 @@ export default defineConfig({
       dts: true,
     }),
   ],
-  build: {
-    outDir: 'C://developer-program/nginx-1.25.4/html/',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
