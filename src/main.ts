03fc8bcb13b5e4b923a72b4e9b864a9ee25d5626
Lee In Seok <dlstjr9068@gmail.com>
2026-01-29 12:04:11 +0900

    패키지 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@/assets/style.css';
 // 2순위 인식
 import '@/assets/custom.css';
 
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
@@ -21,6 +22,7 @@ const router = createRouter({
 
 pinia.use(piniaPluginPersistedstate);
 
+app.use(VueQueryPlugin);
 app.use(router);
 app.use(pinia);
 
