249acffbd99f21c3df9d72c07bba66f9bf0425d2
Lee In Seok <dlstjr9068@gmail.com>
2026-02-08 13:20:59 +0900

    초기 구성

---
+++ apps/portfolio/src/main.ts
@@ -0,0 +1,37 @@
+// 1순위 인식
+import '@/assets/style.css';
+// 2순위 인식
+import '@/assets/custom.css';
+
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
+import { setBasePath } from '@shoelace-style/shoelace';
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/themes/dark.css';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'vue-router/auto-routes';
+import App from './App.vue';
+
+// Shoelace CDN 경로 설정
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/');
+
+const app = createApp(App);
+
+const pinia = createPinia();
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+pinia.use(piniaPluginPersistedstate);
+
+app.use(autoAnimatePlugin);
+app.use(VueQueryPlugin);
+app.use(router);
+app.use(pinia);
+
+app.mount('#app');
---
+++ apps/portfolio/vite.config.ts
@@ -0,0 +1,41 @@
+import Tailwindcss from '@tailwindcss/vite';
+import Vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
+import VueRouter from 'unplugin-vue-router/vite';
+import { defineConfig } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vitejs.dev/config/
+const repoRoot = fileURLToPath(new URL('../..', import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    VueRouter(),
+    VueDevTools(),
+    Vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('sl-'),
+        },
+      },
+    }),
+    Components({
+      dirs: ['src/components/app'],
+      dts: true,
+    }),
+    Tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@packages/ui': fileURLToPath(new URL('../../packages/src/ui', import.meta.url)),
+      '@packages/auth': fileURLToPath(new URL('../../packages/src/auth', import.meta.url)),
+    },
+  },
+  server: {
+    fs: {
+      allow: [repoRoot],
+    },
+  },
+});
